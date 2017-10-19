d9054dfa8d5de4ac84b61b0cfbe6968461759073
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2017-10-19 17:03:37 +0200

    Project setup

---
+++ Client/src/group303_PCSSminiProject_Client/Client.java
@@ -0,0 +1,11 @@
+package group303_PCSSminiProject_Client;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+	public static void main(String[] args) {
+		System.out.println("Client project v1");
+	}
+}
---
+++ Server/src/group303_PCSSminiProject_Server/Server.java
@@ -0,0 +1,11 @@
+package group303_PCSSminiProject_Server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server {
+	public static void main(String[] args) {
+		System.out.println("Server project v1...");
+	}
+}
