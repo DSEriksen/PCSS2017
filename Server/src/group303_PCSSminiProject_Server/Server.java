7c3ca2bb05698d183415591599677a92dfa69abd
Mikkel Brogaard Vittrup <mikkel.b.vittrup@gmail.com>
2017-10-20 12:29:28 +0200

    setup

--- Server/src/group303_PCSSminiProject_Server/Server.java
+++ Server/src/group303_PCSSminiProject_Server/Server.java
@@ -6,6 +6,40 @@ import java.util.*;
 
 public class Server {
 	public static void main(String[] args) {
-		System.out.println("Server project v1...");
+		int port = 3002;
+		
+		try {
+			
+			Server server = new Server(port);
+			server.run();
+		
+		} catch (Exception e) { System.out.println(e); }
+	}
+	
+	private int port;
+	private ServerSocket serverSocket;
+	// collection of clients class 'implements Runnable' void run() {}
+	
+	Server(int _port) throws Exception {
+		this.port = _port;
+		initialise();
+	}
+	
+	private void initialise() throws Exception {
+		//Socket client = socket.accept();
+		//BufferedReader inputServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		//socket.close();
+		serverSocket = new ServerSocket(port);
+		System.out.println("Server started on port " + port);
+	}
+	
+	public void run() throws Exception {
+		while(true) {
+			serverSocket.close();
+		}
+	}
+	
+	public void shutDown() {
+		// shutdown
 	}
 }
