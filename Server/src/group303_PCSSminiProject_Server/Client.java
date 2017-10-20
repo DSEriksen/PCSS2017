bd612419c57d33225cdf0f32d062934775c9d8b6
elpresidentenuevo <31987410+elpresidentenuevo@users.noreply.github.com>
2017-10-20 12:45:41 +0200

    client constructor

--- Server/src/group303_PCSSminiProject_Server/Client.java
+++ Server/src/group303_PCSSminiProject_Server/Client.java
@@ -1,7 +1,15 @@
 package group303_PCSSminiProject_Server;
 
+import java.net.*;
+
 public class Client implements Runnable {
 
+	private Socket connection = null;
+	
+	Client(Socket _connection) {
+		this.connection = _connection;
+	}
+	
 	public void run(){
 		
 	}
